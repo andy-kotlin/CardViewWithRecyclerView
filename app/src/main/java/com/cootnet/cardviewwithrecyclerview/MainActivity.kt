d210fd90d681487c703c1aeddaf4c1fb6b237013
amin <aminuddin01697@gmail.com>
2019-06-06 17:15:39 +0530

    RecyclerView update  with datasets

--- app/src/main/java/com/cootnet/cardviewwithrecyclerview/MainActivity.kt
+++ app/src/main/java/com/cootnet/cardviewwithrecyclerview/MainActivity.kt
@@ -15,16 +15,20 @@ class MainActivity : AppCompatActivity() {
 
         val recyclerView =  findViewById<RecyclerView>(R.id.recylerView)
 
-        val lm:RecyclerView.LayoutManager = LinearLayoutManager(this)
+       // val lm:RecyclerView.LayoutManager = LinearLayoutManager(this)
+        val lm: RecyclerView.LayoutManager = GridLayoutManager(this,2)
         recyclerView.layoutManager = lm
+         val c1 = Card("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/PM_Modi_Portrait%28cropped%29.jpg/200px-PM_Modi_Portrait%28cropped%29.jpg",
+             "PM Modi",
+             "Tea Man")
+        val c2 = Card("https://img.etimg.com/thumb/height-480,width-640,msid-67413262,imgsize-130018/dhoni.jpg",
+            "Ms Dhoni",
+            "Sports Man")
 
-        val data =
-            arrayOf(
-                Card("one", "two", "three"),
-                Card("xyz", "usd", "sdhf")
-            )
 
-        recyclerView.adapter = MyAdapter(this)
+        val dataset = arrayOf(c1, c2)
+
+        recyclerView.adapter =  MyAdapter(this, dataset)
 
 
 
--- app/src/main/java/com/cootnet/cardviewwithrecyclerview/MyAdapter.kt
+++ app/src/main/java/com/cootnet/cardviewwithrecyclerview/MyAdapter.kt
@@ -2,32 +2,51 @@ package com.cootnet.cardviewwithrecyclerview
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 
-class MyAdapter(conText: Context) : RecyclerView.Adapter<RecyclerView.Adapter.MyViewHolder>() {
-
-    override fun onBindViewHolder(p0: RecyclerView.Adapter.MyViewHolder, p1: Int) {
-
-    }
-
-    private val mContext:Context
-
+class MyAdapter(context:Context, dataset:Array<Card>) : RecyclerView.Adapter<MyAdapter. MyViewHolder>(){
+    private  val mContext:Context
+    private  val dataset:Array<Card>
     init {
-        this.mContext = conText
+        this.mContext = context
+        this.dataset = dataset
+    }
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val thumbnail:ImageView
+        val title:TextView
+        val subtitle:TextView
+
+        init {
+            this.thumbnail = view.findViewById(R.id.thumbnail)
+            this.title = view.findViewById(R.id.title)
+            this.subtitle = view.findViewById(R.id.subtitle)
+        }
 
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MyViewHolder {
 
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): MyAdapter.MyViewHolder {
+         val rootView  = LayoutInflater.from(parent.context)
+             .inflate(R.layout.album_card, parent, false)
+
+         return MyViewHolder(rootView)
     }
 
     override fun getItemCount(): Int {
+        return this.dataset.size
 
     }
 
-
-
-    class MyViewHolder {
+    override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
+       Glide.with(mContext).load(dataset[position].getThumbnial()).into(holder.thumbnail)
+        holder.title.text  = dataset[position].getTitle()
+        holder.subtitle.text = dataset[position].getSubtitle()
 
     }
 }
